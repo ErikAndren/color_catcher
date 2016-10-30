86cc1b6ffb16b1b72c28c771064ea09e061fe0f9
Erik Andrén Zachrisson <erik@zachrisson.info>
2016-10-30 17:09:14 +0100

    Initial add

---
+++ color_catcher.ino
@@ -0,0 +1,23 @@
+/*********************************************************************
+Color catcher
+Copyright Erik Zachrisson
+erik@zachrisson.info
+*********************************************************************/
+
+#include <SPI.h>
+#include <Wire.h>
+
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
