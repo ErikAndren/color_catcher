6cc2b243a1f0599ccfb06e81f14b50d76e1a36f9
Erik Andrén Zachrisson <erik@zachrisson.info>
2016-12-12 20:39:24 +0100

    Drive pin

--- color_catcher.ino
+++ color_catcher.ino
@@ -22,18 +22,25 @@ Adafruit_SSD1306 display(OLED_RESET);
 #define OUT_PIN 3
 #define S2_PIN  4
 #define S3_PIN  5
+#define S1_PIN  6
+#define S0_PIN  7
 
 ColorRecognitionTCS230 *tcs230;
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
+  pinMode(S0_PIN, OUTPUT);
+  pinMode(S1_PIN, OUTPUT);
+
+  digitalWrite(S0_PIN, LOW);
+  digitalWrite(S1_PIN, HIGH);
   
   tcs230 = ColorRecognitionTCS230::getInstance();
   tcs230->initialize(OUT_PIN, S2_PIN, S3_PIN);
 
   digitalWrite(LED_PIN, HIGH);
   tcs230->adjustWhiteBalance();
-  digitalWrite(LED_PIN, LOW);
+  //digitalWrite(LED_PIN, LOW);
   
   // initialize with the I2C addr 0x3D (for the 128x64)
   display.begin(SSD1306_SWITCHCAPVCC, DISPLAY_I2C_ADDRESS);
